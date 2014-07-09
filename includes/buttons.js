38734bc075b9eafcdadba7e88c8b2b8d4f63c5e1
Orpaz Goldstein <orpgol@gmail.com>
2014-07-09 17:36:51 +0300

    version3
    almost done…

--- includes/buttons.js
+++ includes/buttons.js
@@ -1,3 +1,9 @@
+
+var curr = "content";  		// current starts with 'content'
+var currMenu = null;		// global var that tells me if a menu item is active
+var addPicIndicator = false;
+var settingsIndicator = false;
+
 $(function() 
 {
 	var active = false;
@@ -6,49 +12,51 @@ $(function()
       .click(function( event ) 
       {
       	if(this.id == 'local' || this.id == 'tourist'){
-     		 	console.log(this.id + " was pressed");		// print pressed type to console
-      	document.getElementById('bigContent').style.display='block';
-      	document.getElementById('content').style.display='none';
-      	if (this.id == 'local'){					// decide wich function to call
-      		if(!active){
-      			$('#bigContent').empty();		// clear content of bigContent
-	      		$("#main-nav > ul > li:first-child").removeClass("currentLink");	// remove class from the second link
-	      		$("#main-nav" ).find( "li:first-child" ).css( "display", "none" );	// make the second link disapear
-	      		$("#main-nav" ).find( "li:nth-child(2)" ).css( "width", "636px" );	// current class width changes to full width
-	      		$("#main-nav li:nth-child(2)").addClass("currentLink");				// add currntLink class to current link.
-	      		active = true;
-      		}
-      		else if(active){
-      			document.getElementById('bigContent').style.display='none';
-      			$("#main-nav > ul > li:nth-child(2)").removeClass("currentLink");
-      			$("#main-nav" ).find( "li:nth-child(2)" ).css( "width", "316px" );
-      			$( "#main-nav" ).find( "li:first-child" ).css( "display", "block" );
-      			document.getElementById('tourist').style.display='block';
-      			document.getElementById('content').style.display='block';
-      			active = false;
-      		}
+     		console.log(this.id + " was pressed");		// print pressed type to console
+     		console.log("curr " + curr);
+	      	document.getElementById('bigContent').style.display='block';
+	      	document.getElementById('content').style.display='none';
+	      	if (this.id == 'local'){					// decide wich function to call
+	      		if(!active){
+	      			$('#bigContent').empty();		// clear content of bigContent
+		      		$("#main-nav" ).find( "li:first-child" ).css( "display", "none" );	// make the second link disapear
+		      		$("#main-nav" ).find( "li:nth-child(2)" ).css( "width", "100%" );	// current class width changes to full width
+		      		active = true;
+		      		$("#main-nav").css("background-image","");
+		      		curr = "bigContent"
+	      		}
+	      		else{
+	      			document.getElementById('bigContent').style.display='none';
+	      			$("#main-nav" ).find( "li:nth-child(2)" ).css( "width", "49.5%" );
+	      			$( "#main-nav" ).find( "li:first-child" ).css( "display", "block" );
+	      			document.getElementById('tourist').style.display='block';
+	      			document.getElementById('content').style.display='block';
+	      			active = false;
+	      			curr = "content"
+	      		}
+	      	}
+	      	if (this.id == 'tourist'){
+	      		if(!active){
+	      			$('#bigContent').empty();
+		      		
+		      		$("#main-nav" ).find( "li:nth-child(2)" ).css( "display", "none" );
+		      		$("#main-nav" ).find( "li:first-child" ).css( "width", "100%" );
+		      		
+		      		active = true;
+		      		curr = "bigContent"
+	      		}
+	      		else{
+	      			document.getElementById('bigContent').style.display='none';
+	      			
+	      			$("#main-nav" ).find( "li:first-child" ).css( "width", "49.5%" );
+	      			$( "#main-nav" ).find( "li:nth-child(2)" ).css( "display", "block" );
+	      			document.getElementById('local').style.display='block';
+	      			document.getElementById('content').style.display='block';
+	      			active = false;
+	      			curr = "content"
+	      		}
+	      	} 		
       	}
-      	if (this.id == 'tourist'){
-      		if(!active){
-      			$('#bigContent').empty();
-	      		$("#main-nav li:nth-child(2)").removeClass("currentLink");
-	      		$("#main-nav" ).find( "li:nth-child(2)" ).css( "display", "none" );
-	      		$("#main-nav" ).find( "li:first-child" ).css( "width", "636px" );
-	      		$("#main-nav > ul > li:first-child").addClass("currentLink");
-	      		active = true;
-      		}
-      		else if(active){
-      			document.getElementById('bigContent').style.display='none';
-      			$("#main-nav li:first-child").removeClass("currentLink");
-      			$("#main-nav" ).find( "li:first-child" ).css( "width", "316px" );
-      			$( "#main-nav" ).find( "li:nth-child(2)" ).css( "display", "block" );
-      			document.getElementById('local').style.display='block';
-      			document.getElementById('content').style.display='block';
-      			active = false;
-      		}
-      	} 		
-      	}
-     
         event.preventDefault();
       });
       
@@ -71,6 +79,7 @@ $(function()
 			});	
 		});
 	});
+	
    	// if user clicks the "local" button - pull data from JSON
    	$('#local').click(function() {
     	index = 0;
@@ -90,5 +99,98 @@ $(function()
 			});	
 		});
    	});
+   	
+   	
+   	$('.back').click(function(){
+   		
+   		console.log("current " + currMenu);
+		document.getElementById('savedLocations').style.display='none';
+		document.getElementById('settings').style.display='none';
+		$("#main-page" ).css( "display", "block" );
+		document.getElementById(curr).style.display='block';
+		$(currMenu).css("background-color","");					// reset css
+		$('#favoriteHolder').empty();
+		settingsIndicator = false;
+		currMenu = null;
+    });
       
+
+    $('footer li a').click(function() {
+    	var str;												
+    	// for(var i =1; i <= 4; i++){							
+//     		str = "footer > nav > ul > li:nth-child(" + i + ") a";
+//     		//$(str).removeClass("currentBottomLink");
+//     		$(str).css("background-color","#2D2D2D");			// this should be in the button function
+//     	}
+    	
+    	
+    	if(this.id == 'savedLocationsButton')
+    	{
+    		console.log("current " + currMenu);
+    		if(currMenu == null || currMenu.id != 'savedLocationsButton')
+			{	
+				$(currMenu).css("background-color","");					// reset css
+				//$("#main-nav" ).css( "display", "none" );	// make the links disapear
+				//$("header" ).css( "display", "none" );		// make the links disapear
+				$("#main-page" ).css( "display", "none" );
+				$("#settings" ).css( "display", "none" );
+				document.getElementById(curr).style.display='none';
+				document.getElementById('savedLocations').style.display='block';
+				$(this).css("background-color","#212121");				// css pressed
+				settingsIndicator = false;
+				currMenu = this;
+				loadFavoritesFromJSON();
+			}
+    	}
+    	
+    	
+    	if(this.id == 'addPictureButton'){
+    		if(addPicIndicator){
+    			$('#addPicture').css("display","none");
+    			$(this).css("background-color","");				// css pressed
+    			addPicIndicator = false;	
+    		}
+    		else{
+    			$('#addPicture').css("display","block");
+    			$(this).css("background-color","#212121");				// css pressed
+    			addPicIndicator = true;	
+    		}    		
+    	}
+    	
+    	if(this.id == 'settingsButton'){
+    		if(!settingsIndicator){
+    			document.getElementById('savedLocations').style.display='none';
+    			document.getElementById('main-page').style.display='none';
+    			document.getElementById('settings').style.display='block';
+    			settingsIndicator = true;
+    			$(currMenu).css("background-color","");					// reset css
+    			$(this).css("background-color","#212121");				// css pressed
+    			$('#favoriteHolder').empty();
+				currMenu = this;
+    		}
+    	}
+    	
+   	});  
+   	
+   	var loadFavoritesFromJSON = function(){
+		var index = 0;
+		console.log("iteration");
+		$.getJSON("includes/db.json",function(data){
+			$.each(data,function(key,val){
+				while(index < val.length){	// this while is running all over the json file
+					if(val[index].favorite){
+						$("#favoriteHolder").append( "<section class='greyRectangle'> </section> <article> <p> " + val[index].name + ", " + val[index].city + "</p> </article>");
+					}
+					index++;
+				}
+							
+			});
+		});
+	}
+     
+});
+
+
+$(window).load(function () {					//splash function
+    $('#splashscreen').fadeOut(500);
 });
--- includes/loadJSON.js
+++ includes/loadJSON.js
@@ -5,6 +5,12 @@ $(document).ready(function(){
 	var namesOfPlaces = [];			// array of places
 	$.getJSON("includes/db.json",function(data){
 		$.each(data,function(key,val){
+			// checks if location's favorite flag is on/off
+			if(val[index].favorite == true){
+				$("header > section:nth-child(3) a").css("background-image", "url(images/new-design/like.png)");
+			}else{
+				$("header > section:nth-child(3) a").css("background-image", "url(images/new-design/unlike-top-button.png)");
+			}
 			while(index < 1){	// this while is running all over the json file
 				$("header > section:nth-child(2)").append("<h2>" + val[index].name + ", " + val[index].city + "</h2>");
 				while(sIndex < val[index].images.length){		// this while is running over each Location in json and takes its parameters
